7644e44d93f4dc0db78d6cf7ba76f686859363b9
MohamedAshrafk <152960924+MohamedAshrafk@users.noreply.github.com>
2024-01-13 16:46:51 +0200

    some generalization and optimization for the solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,55 +7,81 @@ import java.util.Random;
 
 public class Main {
 
+    // you can configure the  number of containers to any number
+    private final static int numberOfContainers = 1500;
+    // you can configure the position of the poison (-1 for setting it to a random position)
+    private final static int setPoisonPositionTo = -1;
+
     public static void main(String[] args) {
 
         Random r = new Random();
 
-        ArrayList<Boolean> containers = new ArrayList<>(Collections.nCopies(128, false));
+        ArrayList<Boolean> containers = new ArrayList<>(Collections.nCopies(numberOfContainers, false));
 
-        int trueIndex = r.nextInt(100);
+        int trueIndex;
+        if (setPoisonPositionTo == -1){
+            trueIndex = r.nextInt(numberOfContainers);
+        } else {
+            trueIndex = setPoisonPositionTo;
+        }
         containers.set(trueIndex, true);
         System.out.println("getArray function: The true index is put in index: " + trueIndex);
 
-        System.out.println(getPoisonPosition(containers));
+        ArrayList<Boolean> positionalArray = getPoisonPosition(containers);
+        System.out.println("The position in array form is: " + positionalArray);
+
+        int index = convertBooleanArrayToInteger(positionalArray);
+        System.out.println("The index of the poison in the array is: " + index);
     }
 
-    private static int getPoisonPosition(List<Boolean> arr) {
-        ArrayList<Boolean> result = new ArrayList<>(Collections.nCopies(7, false));
+    private static int convertBooleanArrayToInteger(List<Boolean> arr) {
+        int resultInt = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            resultInt |= ((arr.get(i) ? 1 : 0) << (arr.size() - i - 1));
+        }
+
+        return resultInt;
+    }
+
+    private static ArrayList<Boolean> getPoisonPosition(List<Boolean> containers) {
+
+        int neededBitsForSearching = (int) Math.ceil(Math.log(containers.size()) / Math.log(2));
+        System.out.println("The needed bits for searching is: " + neededBitsForSearching);
+        ArrayList<Boolean> result = new ArrayList<>(Collections.nCopies(neededBitsForSearching, false));
+
+        ArrayList<Boolean> modifiedContainers = new ArrayList<>(Collections.nCopies((int) Math.pow(2, neededBitsForSearching), false));
+        for (int i = 0; i < containers.size(); i++) {
+            modifiedContainers.set(i, containers.get(i));
+        }
 
         int bitCounter = 0;
-        for (double i = arr.size(); i > 1; i /= 2d) {
-            boolean bitResult = getPoisonIndexInternal(arr, bitCounter);
+        for (int i = 0; i < neededBitsForSearching; i++) {
+            boolean bitResult = getPoisonIndexInternal(modifiedContainers, bitCounter);
             result.set(bitCounter, bitResult);
-            System.out.println(bitResult);
             bitCounter++;
         }
 
-        int index = 0;
-        for (int i = 0; i < result.size(); i++) {
-            index |= ((result.get(i) ? 1 : 0) << (result.size() - i - 1));
-        }
-
-        return index;
+        return result;
     }
 
     private static boolean getPoisonIndexInternal(List<Boolean> arr, int bitNumber) {
 
         int numberOfParts = (int) Math.pow(2, (bitNumber + 1));
-        int numberOfProcessedParts = numberOfParts / 2;
+        int mixingSampleSize = arr.size() / numberOfParts;
 
         boolean bitValue = false;
 
-        for (int i = 0; i < numberOfProcessedParts; i++) {
-            bitValue |= mixContainers(arr, i * (arr.size() / numberOfProcessedParts), arr.size() / numberOfParts);
+        for (int i = 0; i < numberOfParts; i++) {
+            if (i % 2 != 0)
+                bitValue |= mixContainers(arr, i * (arr.size() / numberOfParts), mixingSampleSize);
         }
 
-        return !bitValue;
+        return bitValue;
     }
 
-    private static boolean mixContainers(List<Boolean> arr, int startPosition, int mixingSize) {
+    private static boolean mixContainers(List<Boolean> arr, int startPosition, int mixSize) {
         boolean mixingValue = false;
-        for (int i = 0; i < mixingSize; i++) {
+        for (int i = 0; i < mixSize; i++) {
             mixingValue |= arr.get(startPosition + i);
         }
         return mixingValue;
